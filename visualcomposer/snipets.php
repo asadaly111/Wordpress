b69025ca1fc4ae88922ca506ece01d40da9e659d
Asad Ali <asadaly111@users.noreply.github.com>
2019-09-27 12:52:23 +0500

    Delete Old Files

--- visualcomposer/snipets.php
+++
@@ -1,9 +0,0 @@
-//when visual composer not let you edit your page then replace this function to existing one
-
-html2element: function(html) {
-        var $template, attributes = {},
-            template = html;
-        $template = $(template(this.model.toJSON()).trim()), _.each($template.get(0).attributes, function(attr) {
-            attributes[attr.name] = attr.value
-        }), this.$el.attr(attributes).html($template.html()), this.setContent(), this.renderContent()
-    },
